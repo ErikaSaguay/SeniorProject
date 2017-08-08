910844ed236df21cc006914937056bfe7d961161
ErikaSaguay <ecguaman@email.neit.edu>
2017-08-07 21:20:45 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,42 @@
 var express = require('express');
 var router = express.Router();
 var sql = require('mssql');
-var passwordHash = require('password-hash');
 
-router.get('/home', function(req, res, next) {
-    res.render('index');
-});
 
 router.post('/getAllLogos',function(req, res, next){
+    
     var request = new sql.Request();
+    
     request.query("SELECT fileName,logoID FROM Customer_Logos WHERE customerId = '"+req.body.id+"'", function(err, results){
         if(err)
             console.log("Problem getting user info function");
+        for (var i = 0; i < results.length; i++) {
+            res.render(results[i].logoID);
+        }
+    });
+});
+router.post('/getOneLogo', function(req, res, next) {
+
+    sql.query("SELECT fileName FROM Customer_Logos WHERE logoId =  '"+req.body.id+"'", function(err, results){
+        if(err)
+            console.log("Problem getting single logo function");
         for (var i = 0; i < results.length; i++) {
 
         }
     });
 });
-
-router.post('/logos', function(req, res, next) {
+router.post('/insertLogo', function(req, res, next) {
     
     var request = new sql.Request();
     
-    var string = "INSERT INTO Customer_Logos(customerId,logoName,dateCreated)VALUES ?";
-    
-    var values = [req.body.logoName,req.body.customerId,Date.now()];
-    
-    request.query(string, [values], function (err, result) {
+    request.query("INSERT INTO Customer_Logos(customerId,logoName,dateCreated)VALUES ('"+req.body.logoName+"','"+req.body.customerId+"','"+Date.now()+"')", function (err, result) {
         if (err) throw err;
-        console.log("Number of records inserted: " + result.affectedRows);
+        console.log("Logo was inserted");
+        //should show how many rows were effected;
     });
 });
 
-router.post('/oneLogo', function(req, res, next) {
+router.post('/removeOneLogo', function(req, res, next) {
     var request = new sql.Request();
     request.query( "DELETE FROM Customer_Logos WHERE logoId ='"+req.body.id+"'", function (err, result) {
         if (err) throw err;
@@ -40,5 +44,7 @@ router.post('/oneLogo', function(req, res, next) {
     });
 });
 
-
+router.get('/home', function(req, res, next) {
+    res.render('index');
+});
 module.exports = router;
