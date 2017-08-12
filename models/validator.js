46ca9356480adea6355d8aaa8cb82af5f64de855
ErikaSaguay <ecguaman@email.neit.edu>
2017-08-12 12:11:57 -0400

    Add files via upload

---
+++ models/validator.js
@@ -0,0 +1,29 @@
+var constraints = {
+  username: {
+    presence: true
+  },
+  password: {
+    presence: true,
+    length: {
+      minimum: 6,
+      message: "must be at least 6 characters"
+    }
+  },
+  logoname: {
+      presence:true
+  }
+};
+
+//validate for existing user 
+var isUserUnique = function (){
+    
+};
+//validate login page
+var isEmpty = function(){
+    
+}
+//validate signup page
+function validateSignupInpur(){
+    
+}
+
